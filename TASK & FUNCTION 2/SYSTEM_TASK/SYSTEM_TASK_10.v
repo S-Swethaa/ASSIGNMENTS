47ecba1f1eeea2737200b4930443008a23316953
S-Swethaa <146078708+S-Swethaa@users.noreply.github.com>
2025-09-19 22:06:47 +0530

    Add files via upload

---
+++ TASK & FUNCTION 2/SYSTEM_TASK/SYSTEM_TASK_10.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/11/2025 11:41:11 PM
+// Design Name: 
+// Module Name: SYSTEM_TASK_10
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SYSTEM_TASK_10(
+
+    );
+endmodule
